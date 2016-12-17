a6278c51619599338a6e35e687a50d98c9a1be5b
LaurentGirard <laurent.girard44620@gmail.com>
2016-12-17 09:33:44 +0100

    ajout ActionsEnseignant

---
+++ src/metierEnseignant/ActionsEnseignant.java
@@ -0,0 +1,41 @@
+package metierEnseignant;
+
+import java.util.ArrayList;
+
+import utils.Demande;
+import utils.Enseignant;
+import utils.Enseignement;
+import utils.Module;
+
+public class ActionsEnseignant implements IMetierEnseignement{
+	
+	public ActionsEnseignant(){}
+	
+	public void emettreVoeu(Enseignant ens, Module m, Enseignement e, int h, int p){
+		
+	}
+	public void emettreDemExt(Enseignant ens, String org, int vol){
+		
+	}
+	public void emettreDemSpec(Enseignant ens, String t, int vol){
+		
+	}	
+	
+	public void selectionnerSouhaits(ArrayList<Demande> liste){
+		
+	}
+	
+	public void publierSouhaits(){
+		
+	}
+	
+	// A enlever ?
+	public void afficherDemandes(){
+		
+	}
+	
+	public void consulter(){
+		
+	}
+
+}
--- src/metierEnseignant/IMetierEnseignement.java
+++ src/metierEnseignant/IMetierEnseignement.java
@@ -1,5 +1,18 @@
 package metierEnseignant;
 
-public interface IMetierEnseignement {
+import java.util.ArrayList;
 
+import utils.Demande;
+import utils.Enseignant;
+import utils.Enseignement;
+import utils.Module;
+
+public interface IMetierEnseignement {
+	public void emettreVoeu(Enseignant ens, Module m, Enseignement e, int h, int p);
+	public void emettreDemExt(Enseignant ens, String org, int vol);
+	public void emettreDemSpec(Enseignant ens, String t, int vol);
+	public void afficherDemandes();
+	public void selectionnerSouhaits(ArrayList<Demande> liste);
+	public void publierSouhaits();
+	public void consulter();
 }
