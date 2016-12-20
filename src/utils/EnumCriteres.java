773e912977df824c463a8fc5b4a729d961a214b8
LaurentGirard <laurent.girard44620@gmail.com>
2016-12-20 19:21:57 +0100

    add enum criteres

---
+++ src/utils/EnumCriteres.java
@@ -0,0 +1,21 @@
+package utils;
+
+/**
+ * Classe définissant un type de critères.
+ */
+public enum EnumCriteres {
+    /**
+     * Enseignant.
+     */
+    ENSEIGNANT,
+    
+    /**
+     * ENSEIGNEMENT.
+     */
+    ENSEIGNEMENT,
+    
+    /**
+     * MODULE.
+     */
+    MODULE,
+}
